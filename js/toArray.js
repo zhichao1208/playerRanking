d698b3d07b0077adde9c052bc97e7539e2a18789
Jack Li <jackli@Jack.onefootball.local>
2016-08-30 18:17:08 +0200

    sortBy

---
+++ js/toArray.js
@@ -0,0 +1,9 @@
+app.filter("toArray", function(){
+    return function(obj) {
+        var result = [];
+        angular.forEach(obj, function(val, key) {
+            result.push(val);
+        });
+        return result;
+    };
+});
