03967e3677ee47be7b85e5c7fa7acc59565f51c5
Jack Li <jackli@Jack.onefootball.local>
2016-08-30 17:08:58 +0200

    factory

--- js/trending.js
+++ js/trending.js
@@ -1,6 +1,6 @@
 app.factory('players',['$http',function($http){
   
-  return $http.get('Users/jackli/Desktop/onedata/TrendPlayer/web/json/leagueList.json').success(function(data){
+  return $http.get('./json/leagueList.json').success(function(data){
     
     return data;
   })
