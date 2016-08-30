f57e3c4529523d9f3df0ff57ff26da75cdb3c209
Jack Li <jackli@Jack.onefootball.local>
2016-08-30 17:56:04 +0200

    factory players

--- js/players.js
+++ js/players.js
@@ -1,7 +1,7 @@
 app.factory('players',['$http',function($http){
   
-	cid = "1"
-	url = './json/' + cid + '.json'
+	var cid = "1";
+	var url = './json/' + cid + '.json';
 
 
   return $http.get(url).success(function(data){
