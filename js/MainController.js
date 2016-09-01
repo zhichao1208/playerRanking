38d7fbfbeb82b8fcdee9d661c665a210d3b7e003
Jack Li <jackli@Jack.onefootball.local>
2016-09-01 14:31:13 +0200

    chart done

--- js/MainController.js
+++ js/MainController.js
@@ -1,49 +1,64 @@
 app.controller('MainController', ['$scope', 'leagues','players', function($scope, leagues, players) { 
 
+
+$scope.players = [];
+
   leagues.success(function(data){
    $scope.leagues =data;
   });
 
   players.success(function(data){
    $scope.players =data;
-
-
-  console.log($scope.players[20]);
-
-  var player =  $scope.players[20];
-
-  $scope.labels = [player["name"],player["name"],"Last Match"];
-
-  $scope.series = ["votes"];
-
-  $scope.data = [
-    player["last5Votes"]
-  ];
-
-  $scope.onClick = function (points, evt) {
-    console.log(points, evt);
-  };
-
-  $scope.datasetOverride = [{ yAxisID: 'votes' }];
-
-  $scope.options = {
-    scales: {
-      yAxes: [
-        {
-          id: 'votes',
-          type: 'linear',
-          display: true,
-          position: 'left'
-        }
-      ]
-    }
-  };
-
-
-
-
-
   });
+
+
+$scope.choesdPlayer = {};
+
+$scope.choose = function(player){	
+
+	$scope.choesdPlayer = {};
+ 	
+ 		$scope.choesdPlayer = player;
+
+ 		$scope.chart();
+
+};
+
+
+$scope.chart = function(){	
+
+		  var player = $scope.choesdPlayer
+
+		  console.log(player);
+ 
+		  $scope.labels = ["Day1","Day2","Day3"];
+
+		  $scope.series = ["votes"];
+
+		  $scope.data = [
+		    player["last5Votes"]
+		  ];
+
+		  $scope.onClick = function (points, evt) {
+		    console.log(points, evt);
+		  };
+
+		  $scope.datasetOverride = [{ yAxisID: 'votes' }];
+
+		  $scope.options = {
+		    scales: {
+		      yAxes: [
+		        {
+		          id: 'votes',
+		          type: 'linear',
+		          display: true,
+		          position: 'left'
+		        }
+		      ]
+		    }
+		  };
+
+		};
 
 
 
